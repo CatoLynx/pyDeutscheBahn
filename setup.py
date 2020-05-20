0ef9eec2489c400bea0b9b5503e057c53273a787
Julian Metzler <git@mezgr.de>
2020-05-20 22:42:19 +0200

    Fix missing DS100 station list in PyPi package

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 metadata = {}
 with open('deutschebahn/metadata.py') as f:
-	exec(f.read(), metadata)
+    exec(f.read(), metadata)
 
 setup(
-	name = metadata['name'],
-	version = metadata['version'],
-	description = metadata['description'],
-	license = metadata['license'],
-	author = metadata['author'],
-	author_email = metadata['author_email'],
-	install_requires = metadata['requires'],
-	url = metadata['url'],
-	keywords = metadata['keywords'],
-	packages = find_packages(),
+    name = metadata['name'],
+    version = metadata['version'],
+    description = metadata['description'],
+    license = metadata['license'],
+    author = metadata['author'],
+    author_email = metadata['author_email'],
+    install_requires = metadata['requires'],
+    url = metadata['url'],
+    keywords = metadata['keywords'],
+    packages = find_packages(),
+    package_data = {'': ["ds100.csv"]}
 )
