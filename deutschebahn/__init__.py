c1f4a138e0a88319001b200a9ad86763f2664672
Coookie <57903699+GamingCoookie@users.noreply.github.com>
2022-03-08 16:37:57 +0100

    __init__.py: update copyright date

--- deutschebahn/__init__.py
+++ deutschebahn/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2022 Julian Metzler
+Copyright 2020-2022 Julian Metzler
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
