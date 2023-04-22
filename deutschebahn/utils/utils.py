b41ac23c1778788a2d5b9299a2140f13ffcabf68
Julian Metzler <git@mezgr.de>
2023-04-22 18:23:39 +0200

    Add get_display_delay

--- deutschebahn/utils/utils.py
+++ deutschebahn/utils/utils.py
@@ -55,3 +55,39 @@ def route_from(route, station):
 def route_remove_cancelled(route):
     # Filter all cancelled stops from a route
     return [stop for stop in route if not stop.get('isCancelled')]
+
+
+def get_display_delay(delay, bins):
+    """
+    Calculate displayed delay by rounding into provided bins.
+    Example bins:
+    [
+        # Format: (from, to, display)
+        # -1 as an upper bound means
+        # "perform only lower bounds check"
+        ( 0,  4,  0), # round delays from 0 to 4 minutes to 0
+        ( 5,  9,  5), # round delays from 5 to 9 minutes to 5
+        (10, 14, 10), # etc.
+        (15, 19, 15),
+        (20, 24, 20),
+        (25, 29, 25),
+        (30, 34, 30),
+        (35, 39, 35),
+        (40, 44, 40),
+        (45, 49, 45),
+        (50, 59, 50),
+        (60, 69, 60),
+        (70, 79, 70),
+        (80, 89, 80),
+        (90, 99, 90),
+        (100, -1, -1)
+    ]
+    """
+    for _bin in bins:
+        if _bin[1] == -1:
+            if delay >= _bin[0]:
+                return _bin[2]
+        else:
+            if delay >= _bin[0] and delay <= _bin[1]:
+                return _bin[2]
+    return delay
